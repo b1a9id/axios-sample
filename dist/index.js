cc15acd83ce0a38524b1ca6b387f4a0ae10dab40
Ryosuke Uchitate <u.ryosuke@waja.jp>
2017-09-21 09:36:59 +0900

    配列の内容を指定したtemplateに表示

--- dist/index.js
+++ dist/index.js
@@ -944,22 +944,79 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
 
-__WEBPACK_IMPORTED_MODULE_1_axios___default.a.get(`https://api.github.com/users/b1a9id/repos`).then(function (response) {
-    console.log(response);
-}).catch(function (response) {
-    console.log(response);
-});
-var data = { msg: 'Hello' };
 __WEBPACK_IMPORTED_MODULE_0_vue__["a" /* default */].component('my-component', {
-    template: '<div>TEST{{ msg }}<div>TEST{{ msg }}</div></div>',
-    data: function () {
-        return data;
-    }
+    props: ['comp'],
+    template: '<div>TEST{{ comp.id }}<div>TEST{{ comp.name }}</div></div>'
 });
 
-new __WEBPACK_IMPORTED_MODULE_0_vue__["a" /* default */]({
-    el: '#example'
-});
+var vm = new __WEBPACK_IMPORTED_MODULE_0_vue__["a" /* default */]({
+    el: '.example',
+    data: {
+        repos: [{
+            "id": 67921833,
+            "name": "algorithm-sample",
+            "full_name": "b1a9id/algorithm-sample",
+            "owner": {
+                "login": "b1a9id",
+                "id": 19252181,
+                "avatar_url": "https://avatars0.githubusercontent.com/u/19252181?v=4",
+                "gravatar_id": "",
+                "url": "https://api.github.com/users/b1a9id",
+                "html_url": "https://github.com/b1a9id",
+                "followers_url": "https://api.github.com/users/b1a9id/followers",
+                "following_url": "https://api.github.com/users/b1a9id/following{/other_user}",
+                "gists_url": "https://api.github.com/users/b1a9id/gists{/gist_id}",
+                "starred_url": "https://api.github.com/users/b1a9id/starred{/owner}{/repo}",
+                "subscriptions_url": "https://api.github.com/users/b1a9id/subscriptions",
+                "organizations_url": "https://api.github.com/users/b1a9id/orgs",
+                "repos_url": "https://api.github.com/users/b1a9id/repos",
+                "events_url": "https://api.github.com/users/b1a9id/events{/privacy}",
+                "received_events_url": "https://api.github.com/users/b1a9id/received_events",
+                "type": "User",
+                "site_admin": false
+            }
+        }, {
+            "id": 93992212,
+            "name": "angular-sample",
+            "full_name": "b1a9id/angular-sample",
+            "owner": {
+                "login": "b1a9id",
+                "id": 19252181,
+                "avatar_url": "https://avatars0.githubusercontent.com/u/19252181?v=4",
+                "gravatar_id": "",
+                "url": "https://api.github.com/users/b1a9id",
+                "html_url": "https://github.com/b1a9id",
+                "followers_url": "https://api.github.com/users/b1a9id/followers",
+                "following_url": "https://api.github.com/users/b1a9id/following{/other_user}",
+                "gists_url": "https://api.github.com/users/b1a9id/gists{/gist_id}",
+                "starred_url": "https://api.github.com/users/b1a9id/starred{/owner}{/repo}",
+                "subscriptions_url": "https://api.github.com/users/b1a9id/subscriptions",
+                "organizations_url": "https://api.github.com/users/b1a9id/orgs",
+                "repos_url": "https://api.github.com/users/b1a9id/repos",
+                "events_url": "https://api.github.com/users/b1a9id/events{/privacy}",
+                "received_events_url": "https://api.github.com/users/b1a9id/received_events",
+                "type": "User",
+                "site_admin": false
+            }
+        }]
+        // methods: {
+        // getRepos: function() {
+        //     axios.get(`https://api.github.com/users/b1a9id/repos`)
+        //     .then(response => {
+        //         this.repos = response.data[0];
+        //         console.log(this.repos);
+        //     })
+        //     .catch(function (response) {
+        //         console.log(response);
+        //     });
+        // }
+        // },
+        // mounted: function() {
+        //     console.log('TEST1');
+        //     this.getRepos();
+        //     console.log('TEST2');
+        // }
+    } });
 
 /***/ }),
 /* 9 */
--- src/index.js
+++ src/index.js
@@ -1,21 +1,82 @@
 import Vue from 'vue';
 import axios from 'axios';
 
-axios.get(`https://api.github.com/users/b1a9id/repos`)
-    .then(function (response) {
-        console.log(response);
-    })
-    .catch(function (response) {
-        console.log(response);
-    });
-var data = {msg: 'Hello'};
+
+
 Vue.component('my-component', {
-    template: '<div>TEST{{ msg }}<div>TEST{{ msg }}</div></div>',
-    data: function() {
-        return data;
-    }
+    props:['comp'],
+    template: '<div>TEST{{ comp.id }}<div>TEST{{ comp.name }}</div></div>'
 })
 
-new Vue({
-    el: '#example'
+var vm = new Vue({
+    el: '.example',
+    data: {
+        repos: [
+            {
+                "id": 67921833,
+                "name": "algorithm-sample",
+                "full_name": "b1a9id/algorithm-sample",
+                "owner": {
+                  "login": "b1a9id",
+                  "id": 19252181,
+                  "avatar_url": "https://avatars0.githubusercontent.com/u/19252181?v=4",
+                  "gravatar_id": "",
+                  "url": "https://api.github.com/users/b1a9id",
+                  "html_url": "https://github.com/b1a9id",
+                  "followers_url": "https://api.github.com/users/b1a9id/followers",
+                  "following_url": "https://api.github.com/users/b1a9id/following{/other_user}",
+                  "gists_url": "https://api.github.com/users/b1a9id/gists{/gist_id}",
+                  "starred_url": "https://api.github.com/users/b1a9id/starred{/owner}{/repo}",
+                  "subscriptions_url": "https://api.github.com/users/b1a9id/subscriptions",
+                  "organizations_url": "https://api.github.com/users/b1a9id/orgs",
+                  "repos_url": "https://api.github.com/users/b1a9id/repos",
+                  "events_url": "https://api.github.com/users/b1a9id/events{/privacy}",
+                  "received_events_url": "https://api.github.com/users/b1a9id/received_events",
+                  "type": "User",
+                  "site_admin": false
+                }
+            },
+            {
+                "id": 93992212,
+                "name": "angular-sample",
+                "full_name": "b1a9id/angular-sample",
+                "owner": {
+                  "login": "b1a9id",
+                  "id": 19252181,
+                  "avatar_url": "https://avatars0.githubusercontent.com/u/19252181?v=4",
+                  "gravatar_id": "",
+                  "url": "https://api.github.com/users/b1a9id",
+                  "html_url": "https://github.com/b1a9id",
+                  "followers_url": "https://api.github.com/users/b1a9id/followers",
+                  "following_url": "https://api.github.com/users/b1a9id/following{/other_user}",
+                  "gists_url": "https://api.github.com/users/b1a9id/gists{/gist_id}",
+                  "starred_url": "https://api.github.com/users/b1a9id/starred{/owner}{/repo}",
+                  "subscriptions_url": "https://api.github.com/users/b1a9id/subscriptions",
+                  "organizations_url": "https://api.github.com/users/b1a9id/orgs",
+                  "repos_url": "https://api.github.com/users/b1a9id/repos",
+                  "events_url": "https://api.github.com/users/b1a9id/events{/privacy}",
+                  "received_events_url": "https://api.github.com/users/b1a9id/received_events",
+                  "type": "User",
+                  "site_admin": false
+                }
+            }
+            ]
+    }
+    // methods: {
+        // getRepos: function() {
+        //     axios.get(`https://api.github.com/users/b1a9id/repos`)
+        //     .then(response => {
+        //         this.repos = response.data[0];
+        //         console.log(this.repos);
+        //     })
+        //     .catch(function (response) {
+        //         console.log(response);
+        //     });
+        // }
+    // },
+    // mounted: function() {
+    //     console.log('TEST1');
+    //     this.getRepos();
+    //     console.log('TEST2');
+    // }
 })
